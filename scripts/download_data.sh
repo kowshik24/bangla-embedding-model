0f1a5f97e9a66bd2437c7291aeffa543b3be2f01
kowshik24 <kowshikcseruet1998@gmail.com>
2025-12-10 16:10:40 +0600

    Update dataset loading in download script to remove trust_remote_code flag

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -11,20 +11,20 @@ from datasets import load_dataset
 
 # Download Bangla paraphrase data
 print('Downloading Bangla paraphrase dataset...')
-dataset = load_dataset('csebuetnlp/paraphrase_identification_bn', trust_remote_code=True)
+dataset = load_dataset('csebuetnlp/BanglaParaphrase')
 dataset.save_to_disk('data/raw/paraphrase_bn')
 
 # Download Bangla NLI data (if available)
 print('Downloading Bangla NLI dataset...')
 try:
-    nli_dataset = load_dataset('csebuetnlp/xnli_bn', trust_remote_code=True)
+    nli_dataset = load_dataset('csebuetnlp/xnli_bn')
     nli_dataset.save_to_disk('data/raw/xnli_bn')
 except:
     print('NLI dataset not found, skipping...')
 
 # Download STS benchmark data
 print('Downloading STS data...')
-sts_dataset = load_dataset('mteb/sts12-sts', trust_remote_code=True)
+sts_dataset = load_dataset('mteb/sts12-sts')
 sts_dataset.save_to_disk('data/raw/sts')
 
 print('Download complete!')
